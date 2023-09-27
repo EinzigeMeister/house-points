07e9c01da1044aa5a55ae085f05b5cf7446fa2e0
Eric Krukowski <erickruk@amazon.com>
2023-09-27 03:08:18 -0400

    Move serialization to to_dict()

--- server/app.py
+++ server/app.py
@@ -3,13 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import Family
+from models import Family, User, Task
 
 # Views go here!
 
@@ -17,7 +17,16 @@ from models import Family
 def index():
     return '<h1>Project Server</h1>'
 
+class FamilyList(Resource):
+    def get(self):
+        family_dict = [f.to_dict(only=("id","family_name", "family_username","tasks.id", "users.id")) for f in Family.query.all()]
+        
+        return make_response(
+            family_dict,
+            200
+        )
 
+api.add_resource(FamilyList, '/families', endpoint='/families')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
