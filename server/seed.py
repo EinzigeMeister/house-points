dbeb5f90a7882e4baf309b6400a5e1ac59e3353c
Eric Krukowski <erickruk@amazon.com>
2023-09-26 20:04:35 -0400

    Add Family seed data

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,32 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Family
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        print("deleting tables...")
+        Family.query.delete()
         print("Starting seed...")
+        families = []
+        family_usernames = []
+        for i in range (10):
+            family_name = fake.last_name()
+            family_username = fake.profile()['username']
+            while family_username in family_usernames:
+                family_username = fake.profile()['username']
+            password_hash = fake.password(length=10)
+            
+            new_family = Family(family_name=family_name, family_username=family_username)
+            new_family.password_hash = password_hash
+
+            families.append(new_family)
+
+        db.session.add_all(families)
+        db.session.commit()
+
+        family_list = Family.query.all()
+        for f in family_list:
+            print(f)
         # Seed code goes here!
