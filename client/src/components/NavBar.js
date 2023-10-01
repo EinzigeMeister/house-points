21f9b235f01d64450af09ac37c2b8e86122e88d4
Eric Krukowski <erickruk@amazon.com>
2023-10-01 19:09:01 -0400

    Add BrowserRouter to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter } from "react-router-dom";
 function NavBar() {
-  <div class="container-fluid">
-    <hr style={{ borderTop: "10px solid blue" }} />
-    <div class="row">
-      <Link to="/" class="col-sm">
-        Home
-      </Link>
-      <Link to="/Tasks" class="col-sm">
-        Tasks
-      </Link>
-      <Link to="/Scoreboard" class="col-sm">
-        Score Board
-      </Link>
-    </div>
-    <hr style={{ borderTop: "10px solid blue" }} />
-  </div>;
+  return (
+    <BrowserRouter>
+      <div class="container-fluid">
+        <hr style={{ borderTop: "10px solid blue" }} />
+        <div class="row">
+          <Link to="/" class="col-sm">
+            Home
+          </Link>
+          <Link to="/Tasks" class="col-sm">
+            Tasks
+          </Link>
+          <Link to="/Scoreboard" class="col-sm">
+            Score Board
+          </Link>
+        </div>
+        <hr style={{ borderTop: "10px solid blue" }} />
+      </div>
+    </BrowserRouter>
+  );
 }
 export default NavBar;
