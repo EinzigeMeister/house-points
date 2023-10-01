115daba5a355c38da593b8f5e01140c0134e6fac
Eric Krukowski <erickruk@amazon.com>
2023-09-30 22:44:27 -0400

    Verify successful fetch from python server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [taskList, setTaskList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [family, setFamily] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5555/users").then((r) => r.json().then((userList) => setUsers(userList)));
+  }, []);
+  if (users.length < 1) {
+    return <h1>Project Client</h1>;
+  } else return <h1>Found users!</h1>;
 }
 
 export default App;
