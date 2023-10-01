8e341aabca57adaf5cf9627ce252957d2889d23a
Eric Krukowski <erickruk@amazon.com>
2023-10-01 18:17:03 -0400

    Add minimum bootstrap css files to index

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
