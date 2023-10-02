cf20c34a5d24b62936a83b4fcb7a42851f0694d9
Eric Krukowski <erickruk@amazon.com>
2023-10-01 21:01:49 -0400

    Change Exception to Attribute Error

--- server/models.py
+++ server/models.py
@@ -20,7 +20,7 @@ class Family(db.Model, SerializerMixin):
 
    @hybrid_property
    def password_hash(self):
-      raise Exception("Password hashes may not be viewed.")
+      raise AttributeError("Password hashes may not be viewed.")
    
    @password_hash.setter
    def password_hash(self, password):
