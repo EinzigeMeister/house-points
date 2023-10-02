0b5e4b5ac98be8b14775dff99df185381a43c4ee
Eric Krukowski <erickruk@amazon.com>
2023-10-01 21:00:57 -0400

    Boilerplate ChoreList component

---
+++ client/src/components/ChoreList.js
@@ -0,0 +1,12 @@
+import React from "react";
+/*
+TODO:
+[ ] Generate Family chore list
+[ ] Filter by not-completed
+[ ] 
+ */
+function ChoreList(props) {
+  return <div>Chore List here</div>;
+}
+
+export default ChoreList
