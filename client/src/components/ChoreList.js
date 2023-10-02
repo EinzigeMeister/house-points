ee3fefec46be9c91e9fb1fcaa303f1c92e27de01
Eric Krukowski <erickruk@amazon.com>
2023-10-02 02:10:52 -0400

    Add ChoreList component

--- client/src/components/ChoreList.js
+++ client/src/components/ChoreList.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 /*
 TODO:
 [ ] Generate Family chore list
 [ ] Filter by not-completed
 [ ] 
  */
-function ChoreList(props) {
-  return <div>Chore List here</div>;
+async function getChores(family, setChoreList) {
+  let choreResponse = await fetch("http://127.0.0.1:5555/tasks");
+  let choreJSON = await choreResponse.json();
+  setChoreList(choreJSON);
 }
 
-export default ChoreList
+function ChoreList({ family }) {
+  const [choreList, setChoreList] = useState([]);
+  useEffect(() => {
+    if (family) {
+      getChores(family, setChoreList);
+    }
+  }, []);
+  if (!family) return <div>Login to view Chore List</div>;
+  if (choreList.length > 0) return <div>Chore List here</div>;
+  else return <div>Loading Chores</div>;
+}
+
+export default ChoreList;
