3a0c5bfcdceabe03475ad4a0d9ee8383dce549a7
Eric Krukowski <erickruk@amazon.com>
2023-10-06 03:35:56 -0400

    Edit scoreboard

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ScoreBoard({ family }) {
-  if (family) return <h1>Found User</h1>;
+  if (family) return <h1>COMING SOON!</h1>;
   else return <h1>Login to view your family's score board.</h1>;
 }
 
