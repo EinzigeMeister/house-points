668984b47c8601bb47134e2d9987dc5509945223
Eric Krukowski <erickruk@amazon.com>
2023-10-09 04:35:54 -0400

    Remove home redirect

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function Home({ family }) {
   if (family) return <div>Welcome, members of the {family.family_name} household!</div>;
   else {
-    return <Redirect push to="/login" />;
+    return <div>Login to use features in this application</div>;
   }
 }
 export default Home;
