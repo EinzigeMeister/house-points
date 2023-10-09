7f9ff922b5e6898b9b847615ccbdfd04966d3134
Eric Krukowski <erickruk@amazon.com>
2023-10-09 19:05:49 -0400

    Add authorization for creating new tasks

--- client/src/components/NewChoreForm.js
+++ client/src/components/NewChoreForm.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
-function NewChoreForm({ family }) {
+function NewChoreForm({ family, activeUser }) {
   const [errorMsgs, setErrorMsgs] = useState([]);
-
+  const [disableForm, setDisableForm] = useState(true);
+  useEffect(() => {
+    if (activeUser && activeUser.head_of_household) {
+      if (disableForm) setDisableForm(false);
+    } else if (!disableForm) setDisableForm(true);
+  }, [activeUser, disableForm]);
   const formSchema = yup.object().shape({
     name: yup.string().required("Must enter a name for the chore").max(50),
     description: yup.string().required("Must enter a description for the chore"),
@@ -48,34 +53,39 @@ function NewChoreForm({ family }) {
   if (!family) return <div>Login to add a chore</div>;
   return (
     <div>
+      <p hidden={!disableForm} style={{ color: "red" }}>
+        You must be the head of household to create new chores.
+      </p>
       <p>{errorMsgs.length > 0 ? errorMsgs[0] : ""}</p>
       <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
-        <label htmlFor="name">Chore Name</label>
-        <br />
-        <input id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
-        <p style={{ color: "red" }}> {formik.errors.name}</p>
+        <fieldset disabled={disableForm} style={{ border: "none" }}>
+          <label htmlFor="name">Chore Name</label>
+          <br />
+          <input id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+          <p style={{ color: "red" }}> {formik.errors.name}</p>
 
-        <label htmlFor="description">Chore Description</label>
-        <br />
-        <input id="description" name="description" onChange={formik.handleChange} value={formik.values.description} />
-        <p style={{ color: "red" }}> {formik.errors.description}</p>
+          <label htmlFor="description">Chore Description</label>
+          <br />
+          <input id="description" name="description" onChange={formik.handleChange} value={formik.values.description} />
+          <p style={{ color: "red" }}> {formik.errors.description}</p>
 
-        <label htmlFor="frequency">Frequency</label>
-        <br />
-        <input id="frequency" name="frequency" onChange={formik.handleChange} value={formik.values.frequency} />
-        <p style={{ color: "red" }}> {formik.errors.frequency}</p>
+          <label htmlFor="frequency">Frequency</label>
+          <br />
+          <input id="frequency" name="frequency" onChange={formik.handleChange} value={formik.values.frequency} />
+          <p style={{ color: "red" }}> {formik.errors.frequency}</p>
 
-        <label htmlFor="location">Location</label>
-        <br />
-        <input id="location" name="location" onChange={formik.handleChange} value={formik.values.location} />
-        <p style={{ color: "red" }}> {formik.errors.location}</p>
+          <label htmlFor="location">Location</label>
+          <br />
+          <input id="location" name="location" onChange={formik.handleChange} value={formik.values.location} />
+          <p style={{ color: "red" }}> {formik.errors.location}</p>
 
-        <label htmlFor="points">Points</label>
-        <br />
-        <input type="number" id="points" name="points" onChange={formik.handleChange} value={formik.values.points} />
-        <p style={{ color: "red" }}> {formik.errors.points}</p>
+          <label htmlFor="points">Points</label>
+          <br />
+          <input type="number" id="points" name="points" onChange={formik.handleChange} value={formik.values.points} />
+          <p style={{ color: "red" }}> {formik.errors.points}</p>
 
-        <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
+        </fieldset>
       </form>
     </div>
   );
