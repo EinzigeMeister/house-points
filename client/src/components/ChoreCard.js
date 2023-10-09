0e6eb4c1846060f0642f74b88065e09a80c1df38
Eric Krukowski <erickruk@amazon.com>
2023-10-09 19:06:08 -0400

    Add authorization for completing/removing tasks

--- client/src/components/ChoreCard.js
+++ client/src/components/ChoreCard.js
@@ -1,34 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable eqeqeq */
 import React, { useEffect, useState } from "react";
 
-import { FormControl, InputLabel, Select, MenuItem, Card, Button, CardActions, CardContent, Typography } from "@mui/material";
+import { FormControl, Card, Button, CardActions, CardContent, Typography } from "@mui/material";
 
-function ChoreCard({ chore, family, users }) {
-  const [chosenUser, setChosenUser] = useState("");
-  // const [likedBy, setLikedBy] = useState([]);
-  const [disabled, setDisabled] = useState(false);
+function ChoreCard({ chore, activeUser, refreshPage, setRefreshPage }) {
+  const [disableCompletetask, setDisableCompleteTask] = useState(true);
+  const [disableRemoveTask, setDisableRemoveTask] = useState(true);
   const [disabledColor, setDisabledColor] = useState("Yellow");
   const [taskCompleteText, setTaskCompleteText] = useState("Complete Task");
-  const [refreshPage, setRefreshPage] = useState("false");
   useEffect(() => {
-    if (family != null) {
-      if (users.length === 0) setDisabled(true);
-      if (chore["completed_by_user_id"] != null) {
-        setDisabled(true);
+    if (activeUser) {
+      if (chore.completed_by_user_id) {
         setDisabledColor("Green");
-        const completedUser = users.find((user) => user.id === chore["completed_by_user_id"]);
-        if (completedUser != undefined) setChosenUser(completedUser.id);
+        if (!disableCompletetask) setDisableCompleteTask(true);
+        if (!disableRemoveTask) setDisableRemoveTask(true);
+      } else {
+        if (disableCompletetask) setDisableCompleteTask(false);
+        if (activeUser.head_of_household === true) if (disableRemoveTask) setDisableRemoveTask(false);
       }
+    } else {
+      if (!disableCompletetask) setDisableCompleteTask(true);
+      if (!disableRemoveTask) setDisableRemoveTask(true);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [users, family, chosenUser, refreshPage]);
+  }, [activeUser, refreshPage, setDisableCompleteTask, setTaskCompleteText]);
 
   const { id = 1, location, title, description, points, frequency } = chore;
-  function handleChange(event) {
-    setChosenUser(event.target.value);
-  }
-  function handleCompleteTask(event) {
-    const completedByUserID = chosenUser;
+  function handleCompleteTask() {
+    const completedByUserID = activeUser.id;
     if (completedByUserID === "") return null;
     fetch(`/tasks/family/${id}`, {
       credentials: "include",
@@ -38,15 +37,16 @@ function ChoreCard({ chore, family, users }) {
       },
       body: JSON.stringify({ completed_by_user_id: completedByUserID }),
     });
-    setDisabled(true);
     setTaskCompleteText("Completed");
+    setDisabledColor("Green");
+    setRefreshPage(!refreshPage);
   }
-  function handleDeleteTask(event) {
+  function handleDeleteTask() {
     fetch(`/tasks/family/${id}`, {
       credentials: "include",
       method: "DELETE",
     });
-    setDisabled(true);
+    setDisabledColor("Green");
     setRefreshPage(!refreshPage);
   }
   return (
@@ -64,45 +64,16 @@ function ChoreCard({ chore, family, users }) {
       </CardContent>
       <CardActions>
         <FormControl>
-          <InputLabel id="demo-simple-select-label">Completed By</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={chosenUser}
-            label="Completed By"
-            onChange={handleChange}
-            disabled={disabled}
-          >
-            {users.map((user) => (
-              <MenuItem key={user.id} value={user.id}>
-                {user.name}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button onClick={handleCompleteTask} disabled={disabled}>
+          <Button onClick={handleCompleteTask} disabled={disableCompletetask}>
             {taskCompleteText}
           </Button>
-          <Button onClick={handleDeleteTask} disabled={disabled}>
+          <Button onClick={handleDeleteTask} disabled={disableRemoveTask}>
             Remove Task
           </Button>
         </FormControl>
       </CardActions>
     </Card>
   );
-  // return (
-  //   // <Card style={{ width: "18rem" }}>
-  //   //   <Card.Body>
-  //   //     <Card.Title>{title}</Card.Title>
-  //   //   </Card.Body>
-  //   //   <ListGroup className="list-group-flush">
-  //   //     <ListGroup.Item>ID: {id}</ListGroup.Item>
-  //   //     <ListGroup.Item className="text-capitalize">Description: {description}</ListGroup.Item>
-  //   //     <ListGroup.Item>Frequency: {frequency}</ListGroup.Item>
-  //   //     <ListGroup.Item>Location: {location}</ListGroup.Item>
-  //   //   </ListGroup>
-  //   // </Card>
-  //   <div>ChoreCards</div>
-  // );
 }
 
 export default ChoreCard;
