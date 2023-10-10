34886245318c5eccab3529026d2ae6aab208672b
Eric Krukowski <erickruk@amazon.com>
2023-10-09 21:38:45 -0400

    Add score and task reset functionality

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -4,6 +4,7 @@ import UserScoreCard from "./UserScoreCard";
 import { Button } from "@mui/material";
 function ScoreBoard({ family, users, updateUserList, activeUser }) {
   const [userScores, setUserScores] = useState([]);
+  const [refreshPage, setRefreshPage] = useState(false);
   useEffect(() => {
     if (family && users.length > 0) {
       fetch(`/scoreboard/family/${family.id}`)
@@ -24,9 +25,20 @@ function ScoreBoard({ family, users, updateUserList, activeUser }) {
           }
         });
     }
-  }, [family, users]);
-  function handleSubmit(event) {
-    console.log("hi");
+  }, [family, users, updateUserList, refreshPage]);
+  function handleReset() {
+    //fetch to delete tasks, delete user-likes. keep family and users
+    fetch(`/tasks/family/${family.id}`, {
+      credentials: "include",
+      method: "DELETE",
+    })
+      .then(
+        fetch(`/likes/family/${family.id}`, {
+          credentials: "include",
+          method: "DELETE",
+        })
+      )
+      .then(setRefreshPage(!refreshPage));
   }
   if (!family) return <h1>Login to view your family's score board.</h1>;
   else {
@@ -38,7 +50,7 @@ function ScoreBoard({ family, users, updateUserList, activeUser }) {
             <div hidden={activeUser} style={{ color: "red" }}>
               You must log in from the Change Active Member page to interact with the scoreboard
             </div>
-            <Button onSubmit={handleSubmit} disabled={!activeUser || !(activeUser && activeUser.head_of_household)}>
+            <Button onClick={handleReset} disabled={!activeUser || !(activeUser && activeUser.head_of_household)}>
               Reset scores!
             </Button>
             <div style={{ color: "blue" }}>{activeUser && !activeUser.head_of_household ? "You must be head of household to reset the scores" : ""}</div>
